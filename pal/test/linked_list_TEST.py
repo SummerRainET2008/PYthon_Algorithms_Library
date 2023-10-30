696e578c652fba2079a72c737d1617fe990cb28e
Tian Xia <SummerRainET2008@gmail.com>
2023-10-29 20:35:18 -0700

    new file:   pal/test/__init__.py   new file:   pal/test/linked_list_TEST.py

---
+++ pal/test/linked_list_TEST.py
@@ -0,0 +1,44 @@
+from pal.list.linked_list import LinkedList, ListNode
+import nose
+
+def test_list():
+  list = LinkedList()
+  list.push_front(1)
+  assert list.begin()() == 1
+
+  list.push_back(2)
+  assert list.rbegin()() == 2
+
+  assert list.size() == 2
+
+def test_insertion():
+  list = LinkedList()
+  list.push_back(1)
+  list.push_back(2)
+  list.push_back(3)
+  list.push_back(4)
+  list.insert(list.begin(), ListNode(0))
+  list.insert(list.rbegin(), ListNode(100))
+
+  iter = list.begin()
+  iter = iter.next()
+  list.remove(iter)
+
+  data = []
+  iter = list.begin()
+  while iter is not list.end():
+    data.append(iter())
+    iter = iter.next()
+
+  assert data == [0, 2, 3, 100, 4]
+
+  data = []
+  iter = list.rbegin()
+  while iter is not list.rend():
+    data.append(iter())
+    iter = iter.prev()
+
+  assert data == [0, 2, 3, 100, 4][::-1]
+
+if __name__ == '__main__':
+  nose.run(defaultTest=__name__)
