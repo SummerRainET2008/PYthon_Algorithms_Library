1914a6504f33fbcd23942570922ed9050a5565d3
Tian Xia <SummerRainET2008@gmail.com>
2023-12-24 21:29:35 -0800

    modified:   src/pyal/tree/disjoint_set.py   modified:   src/pyal/tree/disjoint_set_TEST.py

--- src/pyal/tree/disjoint_set.py
+++ src/pyal/tree/disjoint_set.py
@@ -2,17 +2,12 @@
 Author: Tian Xia (TianXia0209@gmail.com)
 '''
 
-
 class DisjointSet:
-  def __init__(self, n):
+  def __init__(self):
     self._fathers = {}
     self._sizes = {}
-    self._clusters_num = n
 
   def combine(self, a, b):
-    assert 0 <= a < self._clusters_num
-    assert 0 <= b < self._clusters_num
-
     c1 = self.get_cluster_id(a)
     c2 = self.get_cluster_id(b)
     if c1 == c2:
@@ -24,17 +19,12 @@ class DisjointSet:
 
     self._fathers[c1] = c2
     self._sizes[c2] = self._sizes.get(c2, 1) + self._sizes.get(c1, 1)
-    self._clusters_num -= 1
 
   def get_cluster_id(self, a):
-    assert 0 <= a < self._clusters_num
-
-    father = self._fathers.get(a, -1)
-    if father == -1:
+    father = self._fathers.get(a, None)
+    if father is None:
       return a
     cluster_id = self.get_cluster_id(father)
     self._fathers[a] = cluster_id
-    return cluster_id
 
-  def get_cluster_num(self):
-    return self._clusters_num
+    return cluster_id
--- src/pyal/tree/disjoint_set_TEST.py
+++ src/pyal/tree/disjoint_set_TEST.py
@@ -5,11 +5,9 @@ import pyal
 
 
 def test_basic():
-  disjoint_set = pyal.DisjointSet(10)
+  disjoint_set = pyal.DisjointSet()
   assert disjoint_set.get_cluster_id(0) == 0
   assert disjoint_set.get_cluster_id(1) == 1
-  assert disjoint_set.get_cluster_num() == 10
 
   disjoint_set.combine(0, 1)
-  assert disjoint_set.get_cluster_num() == 9
   assert disjoint_set.get_cluster_id(0) == disjoint_set.get_cluster_id(1)
